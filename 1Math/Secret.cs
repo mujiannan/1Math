072256f3d0a7353a08421222524d0239d971bf7b
沈楠 <littlesand@outlook.com>
2019-06-20 22:21:49 +0800

    <-- -->

---
+++ 1Math/Secret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Math
+{
+    internal static class Secret
+    {
+        internal static string AzureCognitiveKey="b72b53dd8edd435db758795dc00894d2";
+    }
+}
