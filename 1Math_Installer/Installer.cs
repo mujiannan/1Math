5c7829e79d2f1ddd7341442881f63c292e733758
沈楠 <littlesand@outlook.com>
2019-06-28 18:09:24 +0800

    <-- 完善注册表写入机制 -->

--- 1Math_Installer/Installer.cs
+++ 1Math_Installer/Installer.cs
@@ -9,14 +9,53 @@ using Microsoft.Win32;
 
 namespace _1Math_Installer
 {
-    
-
     class Installer: IReportor
     {
-        public Reportor Reportor { get; }//它只在构造函数中初始化，之后就是只读的（当然，可以令其向外报告事件）
+        public Reportor Reportor { get; }
         public Installer()
         {
             this.Reportor = new Reportor(this);
+        }
+        public async Task StartInstallerAsync()
+        {
+            //进行两步操作（导入证书、注入注册表）
+            //优先选择导入证书，如果证书导入失败，则转而尝试注册表
+            //当两步操作都失败时，向外传递异常信息
+
+            string errInfo = string.Empty;//用于记录错误信息
+            bool trustSuccess = false;//标记证书是否导入成功
+            try
+            {
+                await CertAsync();//导入证书
+                trustSuccess = true;
+            }
+            catch (Exception Ex)
+            {
+                errInfo += Ex.Message;//记录证书导入错误消息
+                Reportor.Report("错误：证书导入失败，请联网并使用管理员权限运行");
+            }
+            if (!trustSuccess)
+            {
+                try//事实上，如果上一步证书没导入成功，再来尝试写注册表也基本上是无济于事的
+                {
+                    Reg.SetTrustPromptBehavior();//写入注册表
+                    trustSuccess = true;
+                }
+                catch (Exception Ex)
+                {
+                    errInfo += Ex.Message;
+                    this.Reportor.Report("错误：注册表写入失败，请尝试使用管理员权限运行");
+                }
+            }
+            if (trustSuccess)//当导入证书与注册表写入有任何一项成功时，都可以正常执行下载、安装的操作
+            {
+                await SetUpAsync();
+            }
+            else
+            {
+                await Task.Delay(2000);//显示最终的错误信息前，留点时间告诉用户要用管理员权限运行
+                throw new Exception(errInfo);//别在本地存日志之类的，直接显示出来吧……
+            }
         }
         private static string Local1MathPath
         {
@@ -34,7 +73,7 @@ namespace _1Math_Installer
         {
             get
             {
-                string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData) + @"\1Math\Downloads\";
+                string path = Local1MathPath + @"\Downloads\";
                 if (!System.IO.Directory.Exists(path))
                 {
                     System.IO.Directory.CreateDirectory(path);
@@ -43,47 +82,6 @@ namespace _1Math_Installer
             }
         }
         
-        public async Task StartInstallerAsync()
-        {
-            //进行两步操作（导入证书、注入注册表）
-            //优先选择导入证书，如果证书导入失败，则转而尝试注册表
-            //当两步操作都失败时，向外传递异常信息
-
-            string errInfo=string.Empty;//用于记录错误信息
-            bool certSuccess=false;//标记证书是否导入成功
-            try
-            {
-                await CertAsync();//导入证书
-                certSuccess = true;
-            }
-            catch(Exception Ex)
-            {
-                errInfo += Ex.Message;//记录证书导入错误消息
-                Reportor.Report("错误：证书导入失败，请联网并使用管理员权限运行");
-            }
-            bool regSuccess = false;//标记注册是否写入成功
-            if (!certSuccess)
-            {
-                try
-                {
-                    Reg.SetTrustPromptBehavior();//写入注册表
-                    regSuccess = true;
-                }
-                catch(Exception Ex)
-                {
-                    errInfo += Ex.Message;
-                    this.Reportor.Report("错误：注册表写入失败，请尝试使用管理员权限运行");
-                }
-            }
-            if (certSuccess|regSuccess)//当导入证书与注册表写入有任何一项成功时，都可以正常执行下载、安装的操作
-            {
-                await SetUpAsync();
-            }
-            else
-            {
-                Reportor.Report(errInfo);
-            }
-        }
         private async Task CertAsync()
         {
             string localFullName = $"{DownloadPath}sn.cer";
@@ -125,30 +123,26 @@ namespace _1Math_Installer
         //当可以导入证书时，最好不要去动注册表
         private static class Reg
         {
-            private static RegistryKey Key
-            {
-                get
-                {
-                    if (System.Environment.Is64BitOperatingSystem)
-                    {
-                        return (Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
-                            @"SOFTWARE\Wow6432Node\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel"));
-                    }
-                    else
-                    {
-                        return (Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
-                            @"SOFTWARE\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel"));
-                    }
-                }
-            }
+
             internal static void SetTrustPromptBehavior()
             {
-                Key.SetValue("MyComputer", "Enabled");
-                Key.SetValue("LocalIntranet", "Enabled");
-                Key.SetValue("Internet", "AuthenticodeRequired");
-                Key.SetValue("TrustedSites", "Enabled");
-                Key.SetValue("UntrustedSites", "AuthenticodeRequired");
-                Key.Close();
+                RegistryKey key;
+                if (System.Environment.Is64BitOperatingSystem)
+                {
+                    key=Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
+                        @"SOFTWARE\Wow6432Node\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel");
+                }
+                else
+                {
+                    key=Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
+                        @"SOFTWARE\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel");
+                }
+                key.SetValue("MyComputer", "Enabled");
+                key.SetValue("LocalIntranet", "Enabled");
+                key.SetValue("Internet", "Enabled");
+                key.SetValue("TrustedSites", "Enabled");
+                key.SetValue("UntrustedSites", "Enabled");
+                key.Close();
             }
         }
 
